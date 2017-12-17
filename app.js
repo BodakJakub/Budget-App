9d897cbb5625dfde5222837149437c4ddf92e79a
BodakJakub <bodakjakub@gmail.com>
2017-12-17 11:28:26 +0100

    Setting up UI module

--- app.js
+++ app.js
@@ -99,6 +99,20 @@ var UIController = (function() {
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
     
+    clearFields: function(){
+      var fields, fieldsArr;
+      
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+      
+      fieldsArr = Array.prototype.slice.call(fields);
+      
+      fieldsArr.forEach(function(current, index, array){
+        current.value = "";
+      });
+      
+      fieldsArr[0].focus();
+    },
+    
     getDOMstrings: function(){
       return DOMstrings;
     }
@@ -136,8 +150,11 @@ var controller = (function(budgetCtrl, UICtrl) {
     // 3. add the item to the UI
     UICtrl.addListItem(newItem, input.type);
     
-    // 4. Calculate the budget
-    // 5. Display the budget
+    // 4. Clear the fields
+    UICtrl.clearFields();
+    
+    // 5. Calculate the budget
+    // 6. Display the budget
     
   };
   
